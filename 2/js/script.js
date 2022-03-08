171ade993370e9b9d66f059747714391c5b05957
Wyndarm <101200463+Wyndarm@users.noreply.github.com>
2022-03-08 20:27:27 +0400

    Update script.js

--- 2/js/script.js
+++ 2/js/script.js
@@ -4,20 +4,6 @@ const monthes = [
 'января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря' 
 ]
 
-// function getDayInfo(date) {
-	
-	// date = date.split(".").map(i => +i);
-	// let t = new Date(date[2],--date[1],date[0]);
-	// let Y = t.getFullYear();
-	// let M = monthes[t.getMonth()];
-	// let D = t.getDate();		
-	// let d = days[t.getDay()];		
-	// let day = Math.ceil(D / 7);	
-	// return `${d}, ${day} неделя ${M} ${Y} года`;
-	
-// }
-
-// console.log(getDayInfo('08.03.2022'));
 
 function getDayInfo(date) {
 	
